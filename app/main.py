e2a1ed64e56a3dd29de9564d5647d7813e3369d5
Ryan H <rharri@users.noreply.github.com>
2023-10-15 02:15:31 -0400

    pass third stage

--- app/main.py
+++ app/main.py
@@ -6,8 +6,13 @@ def main():
     with socket.create_server(("127.0.0.1", 4221), reuse_port=True) as server_socket:
         client_socket, _ = server_socket.accept() # wait for client
         client_data = client_socket.recv(4096)
-        print(client_data)
-        client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        http_request = client_data.decode()
+        http_msg_start_line = http_request.split("\r\n")[0]
+        _, path, _ = http_msg_start_line.split(" ")
+        if path == "/":
+            client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        else:
+            client_socket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
         client_socket.close()
 
 
