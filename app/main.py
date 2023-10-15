8acab9419c55ee4a1c693943fa4d72cee8c46838
Ryan H <rharri@users.noreply.github.com>
2023-10-15 01:38:12 -0400

    fix: receive data from client connection

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     # Create a TCP socket bound to local host
     with socket.create_server(("127.0.0.1", 4221), reuse_port=True) as server_socket:
         client_socket, _ = server_socket.accept() # wait for client
+        client_data = client_socket.recv(4096)
+        print(client_data)
         client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        client_socket.close()
 
 
 if __name__ == "__main__":
